22de36ba4ace64c4f28fd615db24c487a2d6c34c
Ziyi Meng <ziyimeng@s-164-67-215-112.resnet.ucla.edu>
2024-05-22 19:14:07 -0700

    updateServer

--- server.py
+++ server.py
@@ -28,17 +28,14 @@
 # const PORT = 3000;
 # server.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
 
-import socketio
 from flask import Flask, send_from_directory
+from flask_socketio import SocketIO, emit
 from inference import UpdatePosteriorClass
-import eventlet
-import eventlet.wsgi
 
 class GameServer:
     def __init__(self):
-        self.sio = socketio.Server()
         self.app = Flask(__name__, static_url_path='', static_folder='public')
-        self.wsgi_app = socketio.WSGIApp(self.sio, self.app)
+        self.socketio = SocketIO(self.app)
         self.updatePosterior = UpdatePosteriorClass()
         self.allMaps = [
             {
@@ -56,47 +53,42 @@ class GameServer:
                 'goals': [(10, 1), (10, 5), (10, 10)],
                 'blocks': [(5,1),(5,2),(8,2),(5,4),(5,5),(8,5),(8,6),(7,7),(7,8),(7,9)]
             }
-            # Add more maps as required
         ]
 
-        # Register Flask route
         self.app.add_url_rule('/', 'index', self.index)
-
-        # Register event handlers
-        self.sio.on('connect', self.on_connect)
-        self.sio.on('disconnect', self.on_disconnect)
-        self.sio.on('playerReady', self.handle_player_ready)
-        self.sio.on('updatePrior', self.handle_update_prior)
+        self.socketio.on_event('connect', self.on_connect)
+        self.socketio.on_event('disconnect', self.on_disconnect)
+        self.socketio.on_event('playerReady', self.handle_player_ready)
+        self.socketio.on_event('updatePrior', self.handle_update_prior)
 
     def index(self):
         return send_from_directory('public', 'index.html')
 
-    def on_connect(self, sid, environ):
+    def on_connect(self):
         print('A user connected')
 
-    def on_disconnect(self, sid):
+    def on_disconnect(self):
         print('User disconnected')
 
-    def handle_player_ready(self, sid, button):
+    def handle_player_ready(self, button):
         print('player ready')
         game_map = self.onDisplayReady()
-        self.sio.emit('initializeGame', game_map, room=sid)
+        emit('initializeGame', game_map)
 
-    def handle_update_prior(self, sid, newActPos):
+    def handle_update_prior(self, newActPos):
         action = tuple(newActPos['action'])
         print(action)
         posterior = self.updatePosterior(action)
-        self.sio.emit('updatePosterior', posterior, room=sid)
+        emit('updatePosterior', posterior)
 
     def onDisplayReady(self):
-        # Check if there are maps available
         if self.allMaps:
             return self.allMaps.pop(0)
         else:
             return {'error': 'No more maps available'}
 
     def start(self, host='0.0.0.0', port=3000):
-        eventlet.wsgi.server(eventlet.listen((host, port)), self.wsgi_app)
+        self.socketio.run(self.app, host=host, port=port, debug=True)
 
 if __name__ == '__main__':
     server = GameServer()
