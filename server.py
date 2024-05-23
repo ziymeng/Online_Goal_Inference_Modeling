06469c21363c84773656c92fa6618436ed73b024
Ziyi Meng <ziyimeng@s-164-67-215-112.resnet.ucla.edu>
2024-05-22 19:05:40 -0700

    classServer

--- server.py
+++ server.py
@@ -28,40 +28,18 @@
 # const PORT = 3000;
 # server.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
 
-from inference import UpdatePosteriorClass
-
+import socketio
 from flask import Flask, send_from_directory
-from flask_socketio import SocketIO, emit
-from enum import Enum, unique
-import rx, random
-from rx import operators as ops
+from inference import UpdatePosteriorClass
+import eventlet
+import eventlet.wsgi
 
-app = Flask(__name__, static_url_path='', static_folder='public')
-socketio = SocketIO(app)
-
-@app.route('/')
-def index():
-    return send_from_directory('public', 'index.html')
-
-@socketio.on('connect')
-def on_connect():
-    print('A user connected')
-
-@socketio.on('disconnect')
-def on_disconnect():
-    print('User disconnected')
-
-# define a class, called "sendingMaps"
-# in class, have attribute called "allMaps", a list of individual maps
-# also a function called onDisplayReady
-# and inside function, self.allMaps.pop
-
-# pop the first item from the list, and remove the first item in the list
-# so next time calling pop, just call the first item of the remaining list
-
-class SendingMaps:
+class GameServer:
     def __init__(self):
-        # Initialize the `allMaps` attribute with a list of game maps
+        self.sio = socketio.Server()
+        self.app = Flask(__name__, static_url_path='', static_folder='public')
+        self.wsgi_app = socketio.WSGIApp(self.sio, self.app)
+        self.updatePosterior = UpdatePosteriorClass()
         self.allMaps = [
             {
                 'playerPosition': (0, 0),
@@ -81,6 +59,35 @@ class SendingMaps:
             # Add more maps as required
         ]
 
+        # Register Flask route
+        self.app.add_url_rule('/', 'index', self.index)
+
+        # Register event handlers
+        self.sio.on('connect', self.on_connect)
+        self.sio.on('disconnect', self.on_disconnect)
+        self.sio.on('playerReady', self.handle_player_ready)
+        self.sio.on('updatePrior', self.handle_update_prior)
+
+    def index(self):
+        return send_from_directory('public', 'index.html')
+
+    def on_connect(self, sid, environ):
+        print('A user connected')
+
+    def on_disconnect(self, sid):
+        print('User disconnected')
+
+    def handle_player_ready(self, sid, button):
+        print('player ready')
+        game_map = self.onDisplayReady()
+        self.sio.emit('initializeGame', game_map, room=sid)
+
+    def handle_update_prior(self, sid, newActPos):
+        action = tuple(newActPos['action'])
+        print(action)
+        posterior = self.updatePosterior(action)
+        self.sio.emit('updatePosterior', posterior, room=sid)
+
     def onDisplayReady(self):
         # Check if there are maps available
         if self.allMaps:
@@ -88,25 +95,9 @@ class SendingMaps:
         else:
             return {'error': 'No more maps available'}
 
-# Instantiate the SendingMaps class
-map_manager = SendingMaps()
-
-# Define the socket event handler outside the class
-@socketio.on('playerReady')
-def handlePlayerReady(button):
-    print('player ready')
-    game_map = map_manager.onDisplayReady()
-    emit('initializeGame', game_map)
-
-updatePosterior = UpdatePosteriorClass()
-
-@socketio.on('updatePrior')
-def handleNewAct(newActPos):
-    action = tuple(newActPos['action'])
-    print(action)
-    posterior = updatePosterior(action)
-    emit('updatePosterior', posterior)
+    def start(self, host='0.0.0.0', port=3000):
+        eventlet.wsgi.server(eventlet.listen((host, port)), self.wsgi_app)
 
 if __name__ == '__main__':
-    PORT = 3000
-    socketio.run(app, port=PORT, debug=True)
+    server = GameServer()
+    server.start()
